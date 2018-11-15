f3f95d40cbd8f123a2110e8b4f227782cdd992f2
Tapan Modi <tapanr97@gmail.com>
2018-11-16 00:15:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 function changeStyle(prevInd, index, move_no) { 
   var urlString = 'url(' + require('./Capture.PNG') + ')';
@@ -18,6 +20,8 @@ class App extends Component {
     this.state = {
       arr: [],
       dimensions: 6,
+      n: 6,
+      m: 6,
       x: 0,
       y: 0,
       id: 0,
@@ -58,7 +62,7 @@ class App extends Component {
     
     let myid = setInterval(function run(){
       let splt = arr[tmp].split(" ");
-      let ind = parseInt(splt[1]) * parseInt(that.state.dimensions) + parseInt(splt[2]); 
+      let ind = parseInt(splt[1]) * parseInt(that.state.m) + parseInt(splt[2]); 
       changeStyle(prevInd, ind, splt[0]);
       prevInd = ind;
       tmp = tmp + 1;
@@ -80,26 +84,27 @@ class App extends Component {
   }
 
   handleClear = () => {
-    
+    document.getElementById("in0").disabled = '';    
     document.getElementById("in1").disabled = '';
     document.getElementById("in2").disabled = '';
     document.getElementById("in3").disabled = '';     
     document.getElementById("in4").disabled = '';
 
     clearInterval(this.state.id);
-    for(let i = 0; i < this.state.dimensions; i++) {
-      for(let j = 0; j < this.state.dimensions; j++) {
+    for(let i = 0; i < this.state.n; i++) {
+      for(let j = 0; j < this.state.m; j++) {
         if((i % 2) == 1 && (j % 2) == 1)
-          document.getElementById(i * this.state.dimensions + j).style.background = 'white';
+          document.getElementById(i * this.state.m + j).style.background = 'white';
         else if((i % 2) == 1 && (j % 2) == 0)
-          document.getElementById(i * this.state.dimensions + j).style.background = 'black';
+          document.getElementById(i * this.state.m + j).style.background = 'black';
         else if((i % 2) == 0 && (j % 2) == 1)
-          document.getElementById(i * this.state.dimensions + j).style.background = 'black';
+          document.getElementById(i * this.state.m + j).style.background = 'black';
         else  
-          document.getElementById(i * this.state.dimensions + j).style.background = 'white';
-        document.getElementById(i * this.state.dimensions + j).value = '';  
+          document.getElementById(i * this.state.m + j).style.background = 'white';
+        document.getElementById(i * this.state.m + j).value = '';  
       }
     }
+    document.getElementById("in0").value = '';  
     document.getElementById("in1").value = '';
     document.getElementById("in2").value = '';
     document.getElementById("in3").value = '';     
@@ -109,42 +114,67 @@ class App extends Component {
   render() {
     return (
       
-      <div className="dash">
-          <div style={{width: "50%"}}>
-            <h1 style={{color:"yellow", fontSize:"50px"}}> Knight's Tour </h1>
-            <h2 style={{color:"yellow", fontSize:"30px"}}> IT485 – Logic of Inference </h2>
-          </div>
-        <div className="game">
-          <div className="game-board">
-            <Board dimensions = {this.state.dimensions}/>
-          </div>
+      <div style={{textAlign:"center"}}>
 
+        <div style={{textAlign:"center"}}>
+          <h1 style={{color:"yellow", fontSize:"50px"}}> Knight's Tour </h1>
+          <h2 style={{color:"yellow", fontSize:"30px"}}> IT485 – Logic of Inference </h2>
         </div>
-        <div>
-            <label style={{color:"white", fontWeight:"bold"}}>Enter the dimensions: </label>
-            <input type='text' id = "in1" placeholder="Enter The dimensions" name = "dimensions" onChange = {this.handleInput}></input>
+        <div style={{textAlign:"center", marginTop:"20px"}}>
+          <div className="game-board" style={{display:"inline-block"}}>
+            <Board n = {this.state.n} m = {this.state.m}/>
+          </div>
         </div>
-        <div>
-          <label style={{color:"white", fontWeight:"bold"}}>Enter the Starting Point: </label>
-          <input type='text' id = "in2" placeholder="Enter X-coordinate" name = "x" onChange = {this.handleInput}></input>
-          <input type='text' id = "in3" placeholder="Enter Y-coordinate" name = "y" onChange = {this.handleInput}></input>
-        </div>
-        <div>
-            <label style={{color:"white", fontWeight:"bold"}}>Enter the time interval (in ms): </label>
-            <input type='text' id = "in4" placeholder="Enter the time interval" name = "interval" onChange = {this.handleInput}></input>
-        </div>
-        <div>
-            <button onClick={this.handleStart}>
-              Start
-            </button>
-            <button onClick={this.handleClear}>
-              Clear
-            </button>
-        </div>
-        <div>
-          <h2 style={{color:"yellow"}}> Team Members: </h2>    
+        <div style={{display:"inline-block", width:"45%", marginTop:"20px"}}>            
+            <Form>
+              <FormGroup row> 
+                <Label style={{color:"white", fontWeight:"bold", textAlign:"left", fontSize:"20px"}} sm={6}>Enter the dimensions </Label>
+                <Col sm = {3}>
+                  <Input type='text' id = "in0" placeholder="Enter X-dim" name = "n" onChange = {this.handleInput}
+                    style={{float:"left"}}/>
+                </Col>
+                <Col sm = {3}>
+                  <Input type='text' id = "in1" placeholder="Enter Y-dim" name = "m" onChange = {this.handleInput}/>
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label style={{color:"white", fontWeight:"bold", textAlign:"left",fontSize:"20px"}} sm={6}>Enter the Starting Points </Label>
+                <Col sm = {3}>
+                  <Input type='text' id = "in2" placeholder="Enter X-coordinate" name = "x" onChange = {this.handleInput}
+                    style={{float:"left"}}/>
+                </Col>
+                <Col sm = {3}>
+                  <Input type='text' id = "in3" placeholder="Enter Y-coordinate" name = "y" onChange = {this.handleInput}/>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label style={{color:"white", fontWeight:"bold", textAlign:"left",fontSize:"20px"}} sm={6}>Enter the time interval (in ms) </Label>
+                <Col sm = {6}>
+                  <Input type='text' id = "in4" placeholder="Enter the time interval" name = "interval" onChange = {this.handleInput}/>
+                </Col>
+              </FormGroup>
+              <FormGroup row style={{margin:"0 auto"}}>
+                <div className="row" style={{margin:"0 auto"}}>
+                <Col sm = {5}>
+                  <Button color="warning" onClick={this.handleStart}>
+                    Start
+                  </Button>
+                </Col>
+                <Col sm = {2}/>
+                <Col sm = {5}>
+                  <Button color="light" onClick={this.handleClear}>
+                    Clear
+                  </Button>
+                </Col>
+                </div> 
+              </FormGroup>
+            </Form>
+        
+          <h2 style={{color:"yellow", marginTop:"8px"}}> Team Members: </h2>    
           <text style={{fontWeight:"bold", color:"white"}}>1) Tapan Modi (201501199)</text>  <br/>
-          <text style={{fontWeight:"bold", color:"white"}}>1) Pratik Kayastha (201501248)</text>      
+          <text style={{fontWeight:"bold", color:"white"}}>2) Pratik Kayastha (201501248)</text> <br/>
+          <text style={{fontWeight:"bold", color:"white"}}>3) Tejas Kasundra (201501241)</text> <br/>     
         </div>
       </div>
     );
--- src/components/Board.js
+++ src/components/Board.js
@@ -6,19 +6,20 @@ class Board extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        dimensions : 6
+        n : 6,
+        m: 6
       }
     }
   
     renderSquare(i, j) {
-      return <Square value={this.state.dimensions * i + j} x = {i} y = {j}/>;
+      return <Square value={this.state.m * i + j} x = {i} y = {j}/>;
     }
   
     createTable = () => {
       let rows = [];
-      for(let i = 0; i < this.state.dimensions; i++) {
+      for(let i = 0; i < this.state.n; i++) {
         let cols = [];
-        for(let j = 0; j < this.state.dimensions; j++) {
+        for(let j = 0; j < this.state.m; j++) {
           cols.push(this.renderSquare(i, j));
         }
         rows.push(<div className='board-row'>{cols}</div>);
@@ -27,8 +28,11 @@ class Board extends Component {
     }
   
     render() {
-      if(this.props.dimensions > 6) this.state.dimensions = this.props.dimensions;
-      else this.state.dimensions = 6;
+      if(this.props.n > 6 || this.props.m > 6) {
+        this.state.n = this.props.n;
+        this.state.m = this.props.m;
+      }
+      else this.state.n = this.state.m = 6;
       return (
         <div className="board">
           <div>
